83209df9b28f97f4b6ef093d367d30be676a573f
Hackbright Student <no-reply@hackbrightacademy.com>
2017-08-01 13:05:54 -0700

    ratings wip

--- model.py
+++ model.py
@@ -2,70 +2,79 @@
 
 from flask_sqlalchemy import SQLAlchemy
 
-# This is the connection to the PostgreSQL database; we're getting this through
-# the Flask-SQLAlchemy helper library. On this, we can find the `session`
-# object, where we do most of our interactions (like committing, etc.)
+# This is the connection to the PostgreSQL database; we're getting
+# this through the Flask-SQLAlchemy helper library. On this, we can
+# find the `session` object, where we do most of our interactions
+# (like committing, etc.)
 
 db = SQLAlchemy()
-
-
-##############################################################################
-# Model definitions
 
 class User(db.Model):
     """User of ratings website."""
 
     __tablename__ = "users"
-    #inform SQLALchemy that instances of this class will be stored
-    #in a table named users
-    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    #add a column to the tabled named user_id cotaining integers and primary key
+
+    user_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
     email = db.Column(db.String(64), nullable=True)
-    #add a column for email containing strings with optional null columns
     password = db.Column(db.String(64), nullable=True)
     age = db.Column(db.Integer, nullable=True)
     zipcode = db.Column(db.String(15), nullable=True)
 
     def __repr__(self):
         """Provide helpful representation when printed."""
+
         return "<User user_id=%s email=%s>" % (self.user_id,
                                                self.email)
 
 
-# Put your Movie and Rating model classes here.
-
 class Movie(db.Model):
-    """User of ratings website."""
+    """Movie on ratings website."""
 
     __tablename__ = "movies"
-    #inform SQLALchemy that instances of this class will be stored
-    #in a table named users
-    movie_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    #add a column to the tabled named user_id cotaining integers and primary key
-    title = db.Column(db.String(64), nullable=False)
-    #add a column for email containing strings with optional null columns
-    released_at = db.Column(db.DateTime, nullable=True)
-    imdb_url = db.Column(db.String(256), nullable=True)
+
+    movie_id = db.Column(db.Integer,
+                         autoincrement=True,
+                         primary_key=True)
+    title = db.Column(db.String(100))
+    released_at = db.Column(db.DateTime)
+    imdb_url = db.Column(db.String(200))
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<Movie movie_id=%s title=%s>" % (self.movie_id,
+                                                 self.title)
+
 
 class Rating(db.Model):
-    """User of ratings website."""
+    """Rating of a movie by a user."""
 
     __tablename__ = "ratings"
-    #inform SQLALchemy that instances of this class will be stored
-    #in a table named users
-    rating_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    #add a column to the tabled named user_id cotaining integers and primary key
-    movie_id= db.Column(db.Integer, nullable=False)
-    #add a column for email containing strings with optional null columns
-    user_id = db.Column(db.Integer, nullable=False)
-    score = db.Column(db.Integer, nullable=False)
-##############################################################################
+
+    rating_id = db.Column(db.Integer,
+                          autoincrement=True,
+                          primary_key=True)
+    movie_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer)
+    score = db.Column(db.Integer)
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        s = "<Rating rating_id=%s movie_id=%s user_id=%s score=%s>"
+        return s % (self.rating_id, self.movie_id, self.user_id,
+                    self.score)
+
+
+#####################################################################
 # Helper functions
 
 def connect_to_db(app):
     """Connect the database to our Flask app."""
 
-    # Configure to use our PstgreSQL database
+    # Configure to use our PostgreSQL database
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///ratings'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
@@ -73,8 +82,9 @@ def connect_to_db(app):
 
 
 if __name__ == "__main__":
-    # As a convenience, if we run this module interactively, it will leave
-    # you in a state of being able to work with the database directly.
+    # As a convenience, if we run this module interactively, it will
+    # leave you in a state of being able to work with the database
+    # directly.
 
     from server import app
     connect_to_db(app)
